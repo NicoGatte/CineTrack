5b5cb0894918eddafddeb2bdc7736cc2b58234c4
Domenico Nicolò Gattellari <167456475+NicoGatte@users.noreply.github.com>
2026-07-05 13:56:16 +0200

    Delete app directory

--- app/src/main/java/com/cinetrack/app/data/api/dto/GenreDto.kt
+++
@@ -1,9 +0,0 @@
-package com.cinetrack.app.data.api.dto
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class GenreDto(val id: Int, val name: String)
-
-@JsonClass(generateAdapter = true)
-data class GenreListResponse(val genres: List<GenreDto>)
--- app/src/main/java/com/cinetrack/app/domain/model/Genre.kt
+++
@@ -1,6 +0,0 @@
-package com.cinetrack.app.domain.model
-
-data class Genre(
-    val id: Int,
-    val name: String
-)
